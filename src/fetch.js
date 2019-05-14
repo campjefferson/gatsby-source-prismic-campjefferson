f18d5136083006e65c2f7526915bb7e9feaa70c8
Anthony Sapp <anthony.sapp@campjefferson.com>
2019-05-14 10:55:36 -0400

    updated to newest official plugin code

--- src/fetch.js
+++ src/fetch.js
@@ -1,21 +1,21 @@
-import Prismic from 'prismic-javascript'
+import Prismic from 'prismic-javascript';
 
 export default async ({ repositoryName, accessToken, fetchLinks, lang }) => {
-  console.time(`Fetch Prismic data`)
-  console.log(`Starting to fetch data from Prismic`)
+  console.time(`Fetch Prismic data`);
+  console.log(`Starting to fetch data from Prismic`);
 
-  const apiEndpoint = `https://${repositoryName}.prismic.io/api/v2`
-  const client = await Prismic.api(apiEndpoint, { accessToken })
+  const apiEndpoint = `https://${repositoryName}.prismic.io/api/v2`;
+  const client = await Prismic.api(apiEndpoint, { accessToken });
 
   // Query all documents from client
-  const documents = await pagedGet(client, [], { fetchLinks }, lang)
+  const documents = await pagedGet(client, [], { fetchLinks }, lang);
 
-  console.timeEnd(`Fetch Prismic data`)
+  console.timeEnd(`Fetch Prismic data`);
 
   return {
-    documents,
-  }
-}
+    documents
+  };
+};
 
 async function pagedGet(
   client,
@@ -24,20 +24,20 @@ async function pagedGet(
   lang = '*',
   page = 1,
   pageSize = 100,
-  aggregatedResponse = null,
+  aggregatedResponse = null
 ) {
-  const mergedOptions = { lang, ...options }
+  const mergedOptions = { lang, ...options };
 
   const response = await client.query(query, {
     ...mergedOptions,
     page,
-    pageSize,
-  })
+    pageSize
+  });
 
   if (!aggregatedResponse) {
-    aggregatedResponse = response.results
+    aggregatedResponse = response.results;
   } else {
-    aggregatedResponse = aggregatedResponse.concat(response.results)
+    aggregatedResponse = aggregatedResponse.concat(response.results);
   }
 
   if (page * pageSize < response.total_results_size) {
@@ -48,9 +48,9 @@ async function pagedGet(
       lang,
       page + 1,
       pageSize,
-      aggregatedResponse,
-    )
+      aggregatedResponse
+    );
   }
 
-  return aggregatedResponse
+  return aggregatedResponse;
 }
--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,13 +1,13 @@
-import createNodeHelpers from 'gatsby-node-helpers'
-import fetchData from './fetch'
-import { normalizeFields } from './normalize'
+import createNodeHelpers from 'gatsby-node-helpers';
+import fetchData from './fetch';
+import { normalizeFields } from './normalize';
 
-const nodeHelpers = createNodeHelpers({ typePrefix: 'Prismic' })
-const { createNodeFactory } = nodeHelpers
+const nodeHelpers = createNodeHelpers({ typePrefix: 'Prismic' });
+const { createNodeFactory } = nodeHelpers;
 
 export const sourceNodes = async (gatsby, pluginOptions) => {
-  const { actions, createNodeId, store, cache } = gatsby
-  const { createNode, touchNode } = actions
+  const { actions, createNodeId, store, cache } = gatsby;
+  const { createNode, touchNode } = actions;
   const {
     repositoryName,
     accessToken,
@@ -15,19 +15,34 @@ export const sourceNodes = async (gatsby, pluginOptions) => {
     htmlSerializer = () => {},
     fetchLinks = [],
     lang = '*',
-  } = pluginOptions
+    shouldNormalizeImage = () => true,
+    nodeModifier = node => {
+      return node;
+    },
+    onSourceNodesComplete = null
+  } = pluginOptions;
 
   const { documents } = await fetchData({
     repositoryName,
     accessToken,
     fetchLinks,
-    lang,
-  })
+    lang
+  });
 
   await Promise.all(
     documents.map(async doc => {
       const Node = createNodeFactory(doc.type, async node => {
-        node.dataString = JSON.stringify(node.data)
+        const originalNode = node;
+        if (nodeModifier) {
+          node = nodeModifier(node);
+          if (!node) {
+            console.warn(
+              'node not returned from nodeModifier method! using original node ...'
+            );
+            node = originalNode;
+          }
+        }
+        node.dataString = JSON.stringify(node.data);
         node.data = await normalizeFields({
           value: node.data,
           node,
@@ -39,15 +54,18 @@ export const sourceNodes = async (gatsby, pluginOptions) => {
           touchNode,
           store,
           cache,
-        })
+          shouldNormalizeImage
+        });
 
-        return node
-      })
+        return node;
+      });
 
-      const node = await Node(doc)
-      createNode(node)
-    }),
-  )
-
-  return
-}
+      const node = await Node(doc);
+      createNode(node);
+    })
+  );
+  if (onSourceNodesComplete && typeof onSourceNodesComplete === 'function') {
+    onSourceNodesComplete();
+  }
+  return;
+};
